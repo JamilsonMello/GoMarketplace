61d806d76eecae7d116dc75ac77e51040cf6751f
JamilsonMello <jamilsonmelow@gmail.com>
2020-06-01 00:27:20 -0300

    increment and decrement from cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,30 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalCart = products.reduce((accumulator, { price, quantity }) => {
+      accumulator += price * quantity;
 
-    return formatValue(0);
+      return accumulator;
+    }, 0);
+
+    return formatValue(totalCart);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItems = products.reduce(
+      (total, { quantity }) => (total += quantity),
+      0,
+    );
 
-    return 0;
+    return totalItems;
   }, [products]);
 
   return (
